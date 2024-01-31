327cf59fbc5606e220a01c79dc1885fe2fe61fdb
andrew <andrewmekhail@pm.me>
2024-01-31 16:49:16 -0500

    Restructured files and added navbar as a component

--- Lab1/Scripts/app.js
+++ Lab1/Scripts/app.js
@@ -1,7 +1,8 @@
 /*
-Lesson 1
-Names and student Id: Yashvi Patel:  100900705
-                      Justin Wastle:
-                      Andrew Mekhail:
-Date Completed: February 1st, 2024
+Lab 1
+Group Members:
+- Yashvi Patel: 100900705
+- Justin Wastle: 
+- Andrew Mekhail: 100828858
+Date: February 1st, 2024 
 */
