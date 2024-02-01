bca71ce7ef17677b9aa2abe252fe0b1446a898d4
andrew <andrewmekhail@pm.me>
2024-02-01 18:58:52 -0500

    Added spaces after the icons in the navbar

--- Lab1/scripts/components.js
+++ Lab1/scripts/components.js
@@ -24,16 +24,16 @@ class Navbar extends HTMLElement {
                             aria-hidden="true"></i>Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="products.html"><i class="fa fa-th" aria-hidden="true"></i>Products</a>
+                        <a class="nav-link" href="products.html"><i class="fa fa-th" aria-hidden="true"></i> Products</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="services.html"> <i class="fa fa-bell" aria-hidden="true"> </i>Service</a>
+                        <a class="nav-link" href="services.html"> <i class="fa fa-bell" aria-hidden="true"> </i> Service</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="about.html"><i class="fa fa-info-circle" aria-hidden="true"></i>About Us</a>
+                        <a class="nav-link" href="about.html"><i class="fa fa-info-circle" aria-hidden="true"></i> About Us</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="contact.html"><i class="fa fa-phone" aria-hidden="true"></i>Contact us</a>
+                        <a class="nav-link" href="contact.html"><i class="fa fa-phone" aria-hidden="true"></i> Contact us</a>
                     </li>
                     </ul>
                 </div>
