976e903789592e33711f53e4cc62d8c284653118
andrew <andrewmekhail@pm.me>
2024-02-01 18:58:26 -0500

    Added DOM manipulation for the navbar
    - Changed Products to Projects
    - Added Human Resources after Cont

--- Lab1/Scripts/app.js
+++ Lab1/Scripts/app.js
@@ -6,3 +6,36 @@ Group Members:
 - Andrew Mekhail: 100828858
 Date: February 1st, 2024 
 */
+
+// Inject site content
+
+const navbar = document.getElementById("navbarSupportedContent");
+const navbarUl = navbar.querySelector(".navbar-nav");
+const navbarLinks = navbar.getElementsByClassName("nav-link");
+
+// 3b. Change the Products link in the Navbar to Projects
+const productsItem = Array.from(navbarLinks).find(link => link.textContent.trim() == "Products");
+productsItem.innerHTML = '<i class="fa fa-th" aria-hidden="true"></i> Projects';
+
+// 3c. Add another link to the Navbar before Contact Us
+// Create the li element to hold the a element
+const humanResourcesItem = document.createElement("li");
+// Set the class for the li elem
+humanResourcesItem.className = "nav-item";
+
+// Create the a element to hold the link
+const humanResourcesLink = document.createElement("a");
+// Set the class for the elem
+humanResourcesLink.className = "nav-link";
+// Set the href for the elem
+humanResourcesLink.href = "#";
+// Set the innerHTML (icon and label) for the elem
+humanResourcesLink.innerHTML = '<i class="fa fa-handshake-o" aria-hidden="true"></i> Human Resources';
+
+// Append the a elem to the li elem
+humanResourcesItem.appendChild(humanResourcesLink);
+
+// Find the location of the contact us link
+const contactUsLink = Array.from(navbarLinks).find(link => link.textContent.trim() == "Contact us");
+// In the navbar ul, insert the new elem before the contact us link
+navbarUl.insertBefore(humanResourcesItem, contactUsLink.parentNode)
